24613a3f1a7896cf0e82026319b683e6cee571ba
hastyjr <105962575+hastyjr@users.noreply.github.com>
2022-08-30 21:56:29 -0500

    deliverable 2 submission

--- js/charts.js
+++ js/charts.js
@@ -101,3 +101,28 @@ function buildCharts(sample) {
   });
 }
 
+// DELIVERABLE 2: CREATE A BUBBLE CHART
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: ids,
+      y: bubbleValues,
+      text: bubbleLabels,
+      mode: "markers",
+       marker: {
+         size: bubbleValues,
+         color: bubbleValues,
+         colorscale: "Portland" 
+       }
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID"},
+      automargin: true,
+      hovermode: "closest"
+  };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout); 
